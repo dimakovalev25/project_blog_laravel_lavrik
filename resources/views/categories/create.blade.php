c297bd0ed2b29740d100b0b4b3331f3ac5f26832
dimakovalev <d.nozdryn@gmail.com>
2023-06-13 17:36:36 +0300

    newinput.blade.php newinput.php added

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -6,14 +6,19 @@
         <form action="{{ route('category.store') }}" method="post">
             @csrf
 
-            <x-controls.input name="title" type="text" label="Title:"></x-controls.input>
-            <br/>
 
-            <x-controls.input name="slug" type="text" label="Slug:"></x-controls.input>
-            <br/>
+        <x-controls.newinput id=4 name="title" type="text" label="Title:" defval=""></x-controls.newinput>
+        <x-controls.newinput id=4 name="slug" type="text" label="Slug:" defval=""></x-controls.newinput>
+        <x-controls.newinput id=4 name="description" type="text" label="Description:" defval=""></x-controls.newinput>
 
-            <x-controls.input name="description" type="text" label="Description:"></x-controls.input>
-            <br/>
+{{--            <x-controls.input name="title" type="text" label="Title:"></x-controls.input>--}}
+{{--            <br/>--}}
+
+{{--            <x-controls.input name="slug" type="text" label="Slug:"></x-controls.input>--}}
+{{--            <br/>--}}
+
+{{--            <x-controls.input name="description" type="text" label="Description:"></x-controls.input>--}}
+{{--            <br/>--}}
 
 
             <button class="btn btn-primary" type="submit">send</button>
