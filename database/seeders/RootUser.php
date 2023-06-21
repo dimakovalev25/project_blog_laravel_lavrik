0ce5c3107a0fb0016b3c2313786a02065ce1b78d
dimakovalev <d.nozdryn@gmail.com>
2023-06-21 16:19:57 +0300

    used seeder

--- database/seeders/RootUser.php
+++ database/seeders/RootUser.php
@@ -5,10 +5,22 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class RootUser extends Seeder
 {
     public function run(): void
     {
+
+        $data = [
+            'name' => 'admin',
+            'email' => 'a@a',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin')
+        ];
+
+        User::create($data);
 
     }
 }
